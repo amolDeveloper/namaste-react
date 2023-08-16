07452e7c35f35faf0272ff9f3244cfcec317ec02
Kodge <Amol.Kodge@capita.com>
2023-08-16 10:31:31 +0530

    Episode: 1

---
+++ App.js
@@ -0,0 +1,15 @@
+// const heading = React.createElement("h1", {id: "header"}, "Welcome to React Course");
+// const root = ReactDOM.createRoot(document.getElementById("root"));
+// root.render(heading);
+
+const parent = React.createElement("div", {id: "parent"}, [React.createElement("div", {id: "child1"},[
+    React.createElement("h1", {id: "header1"}, "Welcome to React"),
+    React.createElement("h2", {id: "header2"}, "By AMOL KODGE")]
+),React.createElement("div", {id: "child2"},[
+    React.createElement("h1", {id: "header1"}, "Welcome to React"),
+    React.createElement("h2", {id: "header2"}, "By AMOL KODGE")]
+)]);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(parent);
